d97d7bdfb7ab64985b8adc7a9fb49751f6d88a9a
David Chrystal <davidchrystal@users.noreply.github.com>
2023-12-30 14:26:56 +0000

    Add EntityFramework Core

---
+++ GameStore.Api/Data/DataExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data;
+
+public static class DataExtensions
+{
+    public static void MigrateDb(this IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+        dbContext.Database.Migrate();
+    }
+}
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,9 +1,13 @@
 using GameStore.Api;
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>();
 var connString = builder.Configuration.GetConnectionString("GameStoreContext");
+builder.Services.AddSqlServer<GameStoreContext>(connString);
+
 var app = builder.Build();
+app.Services.MigrateDb();
 app.MapGamesEndpoints();
 app.Run();
